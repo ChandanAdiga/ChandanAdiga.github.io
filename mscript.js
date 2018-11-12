90479127c8a823a5d66d9a2cf9db738db0da19bf
Adiga <look4chandu@gmail.com>
2018-11-13 00:03:41 +0530

    Tabbed View and Footer logos

--- mscript.js
+++ mscript.js
@@ -1,3 +1,17 @@
-function greetMe() {
-    alert('Hello there!');
+function onWindowLoad() {
+    document.getElementById("tab_android").click();
 };
+
+function openProfile(evt, profileName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(profileName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
